5c83c1873f0f98162c4da0a354b4f7861f27e8b0
pyo_awndi <154031276+piyo12052004@users.noreply.github.com>
2026-07-03 06:17:56 +0700

    test ci/cd

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $totalBarangMasuk  = $data['totalBarangMasuk'];
 $totalBarangKeluar = $data['totalBarangKeluar'];
 
 require_once __DIR__ . "/Views/Layout/header.php";
-
+ 
 ?>
  
 <div class="p-6 bg-gray-100 min-h-screen">
